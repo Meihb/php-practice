11eb6da8cb9da8a26457504d7622703543fb1dd0
harbor.M <1253880904@qq.com>
2019-08-05 17:34:16 +0800

    binarysearch

--- algorithm/sorting/Search/BinarySearch.php
+++ algorithm/sorting/Search/BinarySearch.php
@@ -10,21 +10,22 @@
 
 
 /**
+ * 迭代版本
  * @param array $arr
  * @param int $neddle
  * @return bool
  */
-function binartSearch(array $arr, int $neddle)
+function binarySearch(array $arr, int $needle)
 {
     $low = 0;
     $high = count($arr) - 1;
 
     while ($low <= $high) {
         $middle = intval(($low + $high) / 2);
-        echo "low:$low,high:$high,middle:$middle,value:" . $arr[$middle]."<br>";
-        if ($neddle > $arr[$middle]) {
+        echo "low:$low,high:$high,middle:$middle,value:" . $arr[$middle] . "<br>";
+        if ($needle > $arr[$middle]) {
             $low = $middle + 1;
-        } elseif ($neddle < $arr[$middle]) {
+        } elseif ($needle < $arr[$middle]) {
             $high = $middle - 1;
         } else {
             return true;
@@ -34,5 +35,20 @@ function binartSearch(array $arr, int $neddle)
     return false;
 }
 
+function binarySearchRecursion(array $arr, int $needle, int $low, int $high)
+{
+    if ($high < $low) return false;
+
+    $middle = (int)(($high + $low) / 2);
+
+    if ($arr[$middle] < $needle) {
+        return binarySearchRecursion($arr, $needle, $middle + 1, $high);
+    } elseif ($arr[$middle] > $needle) {
+        return binarySearchRecursion($arr, $needle, $low, $middle - 1);
+    } else {
+        return true;
+    }
+}
+
 $arr = [1, 3, 4, 6, 7, 8, 10, 13, 14, 18, 19, 21, 24, 37, 40, 45, 71];
-var_dump(binartSearch($arr, 1));
+var_dump(binarySearch($arr, 13));
