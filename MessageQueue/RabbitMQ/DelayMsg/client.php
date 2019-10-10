c2bc1e7da763514f42985ca28c83846c5b4bcc97
harbor.M <1253880904@qq.com>
2019-10-10 17:04:40 +0800

    what

---
+++ MessageQueue/RabbitMQ/DelayMsg/client.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: meihaibo
+ * Date: 2019/10/10
+ * Time: 15:43
+ */
+
+//todo
---
+++ MessageQueue/RabbitMQ/transaction/server.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: meihaibo
+ * Date: 2019/10/10
+ * Time: 15:43
+ */
